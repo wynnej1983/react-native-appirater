d1121398f3b34bfd3a36a28c747c31ff6037d420
Jonathan Wynne <wynnej1983@gmail.com>
2015-12-14 12:17:27 +0900

    initial commit

---
+++ RNAppirater/RNAppirater.h
@@ -0,0 +1,5 @@
+#import "RCTBridgeModule.h"
+
+@interface RNAppirater : NSObject<RCTBridgeModule>
+
+@end
---
+++ RNAppirater/RNAppirater.m
@@ -0,0 +1,12 @@
+#import "RNAppirater.h"
+
+@implementation RNAppirater
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD((void) setAppId:(NSString*)appId)
+{
+  
+}
+
+@end
